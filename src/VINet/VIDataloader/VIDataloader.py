8da2852a812cceb7585407a6b4af71d272574037
Lee Sehyeon <lifethis21@kookmin.ac.kr>
2024-05-10 19:34:04 +0900

    update: train data shape 수정

--- src/VINet/VIDataloader/VIDataloader.py
+++ src/VINet/VIDataloader/VIDataloader.py
@@ -9,22 +9,35 @@ class VIDataloader(Dataset):
         self.image_dir = image_dir
         self.mask_dir = mask_dir
         self.transform = transform
-        self.images = os.listdir(image_dir)
+        self.images = sorted(os.listdir(image_dir))
 
     def __len__(self):
         return len(self.images)
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.image_dir, self.images[idx])
-        mask_path = os.path.join(self.mask_dir, self.images[idx].replace("jpg", "png"))
-        image = Image.open(img_path).convert("RGB")
-        mask = Image.open(mask_path).convert("L")  # gray scale
+        start_idx = idx - 5
+        end_idx = idx
 
-        if self.transform is not None:
-            image = self.transform(image)
-            mask = self.transform(mask)
+        images = []
+        masks = []
 
-        return image, mask
+        for i in range(start_idx, end_idx):
+            img_path = os.path.join(self.image_dir, self.images[max(i, 0)])
+            mask_path = os.path.join(self.mask_dir, self.images[max(i, 0)].replace("jpg", "png"))
+            image = Image.open(img_path).convert("RGB")
+            mask = Image.open(mask_path).convert("L")  # gray scale
+
+            if self.transform is not None:
+                image = self.transform(image)
+                mask = self.transform(mask)
+
+            images.append(image)
+            masks.append(mask)
+
+        images = torch.stack(images, dim=0).permute(1,0,2,3)
+        masks = torch.stack(masks, dim=0).permute(1,0,2,3)
+
+        return images, masks
 
 
 def get_dataloader(image_dir, mask_dir, batch_size, transform=None):
